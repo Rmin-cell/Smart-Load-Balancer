29a002c0545e15538364a1689f6b04eb546f5b76
Rmin-cell <momeni.armin82@gmail.com>
2024-07-30 21:08:31 +0330

    forget about comments

--- solution/src/Flask.py
+++ solution/src/Flask.py
@@ -3,20 +3,20 @@ import yaml
 import itertools
 import random
 
-# Load config
+
 def load_config(file_path):
     with open(file_path, 'r') as file:
         config = yaml.safe_load(file)
     return config
 
-# Initialize Flask app
+
 app = Flask(__name__)
 
-# Load and parse the YAML file
+
 config_file_path = '/config.yaml'
 config = load_config(config_file_path)
 
-# Extract addresses and weights based on types
+
 def create_weighted_list(org_addresses):
     weighted_list = []
     for address in org_addresses:
@@ -34,7 +34,7 @@ for org, org_addresses in config.get('organizations', {}).items():
         'round_robin': itertools.cycle(addresses),
         'weighted': itertools.cycle(weighted_addresses)
     }
-# Extract all addresses and weights for default handling
+
 all_addresses = [list(address.keys())[0] for org_addresses in config['organizations'].values() for address in org_addresses if isinstance(address, dict)]
 all_weighted_addresses = create_weighted_list([address for org_addresses in config['organizations'].values() for address in org_addresses])
 all_addresses_iterator = {
@@ -42,7 +42,7 @@ all_addresses_iterator = {
     'weighted': itertools.cycle(all_weighted_addresses)
 }
 
-# Endpoint to handle GET requests
+
 @app.route('/', methods=['GET'])
 def handle_request():
     req_type = request.args.get('type')
@@ -61,13 +61,13 @@ def handle_request():
         else:
             target_address = next(all_addresses_iterator['round_robin'])
     
-    # Simulate response from the target address
+    
     response = {
         "message": "Request handled by",
         "address": target_address
     }
     return jsonify(response)
 
-# Run the Flask app
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
